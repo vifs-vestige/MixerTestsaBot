2a5636dfce35e95fa5ebc28fa5c84c8bdfceb17c
vifs <vifs888@gmail.com>
2019-11-09 16:07:43 -0700

    got logic flow set up, just need to copy over logic methods from old project

---
+++ MixerTestsaBot/MixerTestsaBot/ChatBot.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MixerTestsaBot {
+    class ChatBot {
+        private Dictionary<string, List<string>> Brain = new Dictionary<string, List<string>>();
+        private Dictionary<string, List<string>> Quotes = new Dictionary<string, List<string>>();
+        private string Uri;
+        private string BrainFile = "brain.txt";
+        private string QuoteFile = "quote.txt";
+
+        public ChatBot() {
+            Uri = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Brain = Load(BrainFile);
+            Quotes = Load(QuoteFile);
+        }
+
+        public string? parseInput(string message) {
+            string? output = null; 
+            if(message.StartsWith("!")) {
+                if(message.ToLower().StartsWith("!quote")) {
+                    output = ParseQuoteCommand(message);
+                }
+            } else {
+                UpdateBrain(message);
+                if (message.ToLower().Contains(Settings.Default.BotName)) {
+                    output = OutputBrain();
+                }
+            }
+            return output;
+        }
+
+        #region quotes
+
+        private string? ParseQuoteCommand(string message) {
+            string? output = null;
+            var info = message.Split(' ');
+            if(info.Count() == 1) {
+                output = GetQuote();
+            } else if (info.Count() == 2) {
+                output = GetQuote(info[1]);
+            } else {
+                SaveQuote(message);
+            }
+            return output;
+        }
+
+        private string GetQuote(string? User = null) {
+            return "quotes" + User;
+        }
+
+        private void SaveQuote(string message) {
+
+        }
+
+        #endregion
+
+        #region brain
+        private void UpdateBrain(string message) {
+            var inputSplit = message.Split(' ');
+            for (int i = 0; i < (inputSplit.Count() - 2); i++) {
+                var key = inputSplit[i] + ' ' + inputSplit[i + 1];
+                var value = inputSplit[i + 2];
+                Brain = addToInputs(key, value, Brain);
+            }
+            Save(BrainFile, Brain);
+        }
+
+        private string OutputBrain() {
+            return "brain";
+        }
+
+        #endregion
+
+
+        private Dictionary<string, List<string>> addToInputs(string key, string value, Dictionary<string, List<string>> item) {
+            if (item.Keys.Contains(key)) {
+                item[key].Add(value);
+            }
+            else {
+                item.Add(key, new List<string>() { value });
+            }
+            return item;
+        }
+
+        #region save/load files
+
+        private Dictionary<string, List<string>> Load(string path) {
+            var item = new Dictionary<string, List<string>>();
+            if (File.Exists(Path.Combine(Uri, path))) {
+
+                using (StreamReader inputFile = new StreamReader(Path.Combine(Uri, path))) {
+                    var text = inputFile.ReadToEnd();
+                    item = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(text);
+                }
+                return item;
+            } else {
+                return item;
+            }
+
+        }
+
+        private void Save(string path, Dictionary<string, List<string>> item) {
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(Uri, path))) {
+                var text = JsonConvert.SerializeObject(item, Formatting.None);
+                outputFile.Write(text);
+            }
+        }
+        #endregion
+    }
+}
--- MixerTestsaBot/MixerTestsaBot/MainWindow.xaml.cs
+++ MixerTestsaBot/MixerTestsaBot/MainWindow.xaml.cs
@@ -1,28 +1,88 @@
-﻿using System;
+﻿using Mixer.Base;
+using Mixer.Base.Clients;
+using Mixer.Base.Model.Channel;
+using Mixer.Base.Model.Chat;
+using Mixer.Base.Model.OAuth;
+using Mixer.Base.Model.User;
+using Mixer.Base.Web;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace MixerTestsaBot
-{
+namespace MixerTestsaBot {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        private MixerConnection Connection;
+        private ExpandedChannelModel Channel;
+        private PrivatePopulatedUserModel User;
+        private ChatClient ChatClient;
+
+        private ObservableCollection<ChatMessage> ChatMessages = new ObservableCollection<ChatMessage>();
+        private ObservableCollection<ChatUser> ChatUsers = new ObservableCollection<ChatUser>();
+
+        private ChatBot Bot;
+
+        public MainWindow() {
             InitializeComponent();
+
+            Bot = new ChatBot();
+        }
+
+        private async void Login_Click(object sender, RoutedEventArgs e) {
+            Connection = await MixerConnection.ConnectViaLocalhostOAuthBrowser(Settings.Default.ClientId, GetScope());
+
+            if (Connection != null) {
+                User = await Connection.Users.GetCurrentUser();
+                Channel = await Connection.Channels.GetChannel(User.username);
+                ChatClient = await ChatClient.CreateFromChannel(Connection, Channel);
+
+                ChatClient.OnMessageOccurred += ChatClient_MessageOccurred;
+
+                if (await ChatClient.Connect() && await ChatClient.Authenticate()) {
+                    Log("connected");
+                }
+            }
+        }
+
+        private void ChatClient_MessageOccurred(object sender, ChatMessageEventModel e) {
+            var message = new ChatMessage(e);
+            Log(message.UserName + ": " + message.Message);
+            var output = Bot.parseInput(message.Message);
+            if(output != null) {
+                ChatClient.SendMessage(output);
+            }
+        }
+
+        public void Log(string stuff) {
+            ChatOutput.Text += stuff + "\n";
+        }
+
+        private List<OAuthClientScopeEnum> GetScope() {
+            return new List<OAuthClientScopeEnum>()
+{
+                OAuthClientScopeEnum.chat__chat,
+                OAuthClientScopeEnum.chat__connect,
+
+                OAuthClientScopeEnum.channel__details__self,
+                OAuthClientScopeEnum.channel__update__self,
+
+                OAuthClientScopeEnum.user__details__self,
+                OAuthClientScopeEnum.user__log__self,
+                OAuthClientScopeEnum.user__notification__self,
+                OAuthClientScopeEnum.user__update__self
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            ChatClient.SendMessage("test from window");
         }
     }
 }
